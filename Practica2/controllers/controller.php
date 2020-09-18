b32e2e6f0d888500d0b3f9f4863c012b97c53b55
RubénDaríoRamírezPineda <46793054+Broadcast123@users.noreply.github.com>
2020-09-18 12:50:59 -0500

    Practica2Avance4

--- Practica2/controllers/controller.php
+++ Practica2/controllers/controller.php
@@ -35,10 +35,47 @@
                 $respuesta = Datos::ingresoUsuarioModel($datosController,"usuarios");
 
                 //Recibe respuesta del modelo
+                
             }
         }
             
             
         }
+        
+
+
+        //METODO VISTA USUARIOS
+        public function vistaUsuarioController(){
+            $respuesta = Datos::vistaUsuariosModel>"usuarios"
+            
+            foreach ($respuesta as $row => $item)
+                echo '<tr>
+                        <td>'.$item["usuario"].'</td>
+                        <td>'.$item["contrasena"].'</td>
+                        <td>'.$item["email"].'</td>
+                        //columna para editar
+                        <td>'.href= "index.php?action=editar&id='.$item["id"].'".><button> EDITAR'</button></a></td>
+                        //columna para borrar
+                        <td>'.href= "index.php?action=usuarios&idBorrar='.$item["id"].'".><button> EDITAR'</button></a></td>'
+        }
+
+        //METODO EDITAR USUARIOS
+        public function ecitarUsuarioController({
+            //Solicitar el id del usuario a editar
+            $datosController = $_GET["id"];
+            //Enviamos al modelo del id para hacer la consulta y obtener sus datos
+            $respuesta = Datos::editarUsuarioController(){
+                
+                //Recibimos respuesta del modelo e IMPRIMIMOS UNA FORMA PARA EDITAR
+                echo '<input type="hidden" value="'.$respuesta["id"].'name="idEditar">
+                    '<input type="text" value="'.$respuesta["id"].'name="idEditar">
+
+                
+
+            }
+        })
+
     }
+
+
 ?>
--- Practica2/models/crud.php
+++ Practica2/models/crud.php
@@ -4,6 +4,36 @@
         class Datos extends Conexion{
             //Metodo del modelo de registro de usuario (Recibe datos del controlador)
             public function registroUsuarioModel ($datosModel, $tabla)
+
+            //METODO INGRESO USUARIO
+            public function ingresoUsuarioModel($datosModel,$tabla){
+                //Preparamos el PDO
+                $stmt=Conexion::conectar()->prepare("SELECT usuario, contrasena FROM $tabla WHERE usuario = :usuario");
+                //Recibimos el valor "usuario" desde el array almacenado del controlador
+                $stmt->bindParam(":usuario", $datosModel["usuario"],PDO::PARAM_STR);
+                //ejecutamos la consulta en PDO
+                $sstm->excute();
+                //Retornamos el fetch que es el que obtiene una fila o posici[on de un array
+                return $stnt->fetch();
+                //Cerramos el PDO
+                sstm->close();          
+        }
+
+
+
+
+
+
+
+
+
+            //METODO PARA VISTA USUARIO (TABLA)
+            public function vistaUsuariosModel($tabla){
+                $stmt = Conexion::conectar()->prepare("SELECT id, usuario, contrasena, email FROM $tabla");
+                $stmt->execute();
+                return $stnt->fetchAll();
+                $stmt->close;
+            }
         
     }
 ?>
