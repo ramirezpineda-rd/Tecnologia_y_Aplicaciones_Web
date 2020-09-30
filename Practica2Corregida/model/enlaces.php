64306d7ebc7f19f5ee6f1b6ff2ec187f44ea60dc
RubénDaríoRamírezPineda <46793054+Broadcast123@users.noreply.github.com>
2020-09-30 10:23:46 -0500

    Practica2Avance11

--- Practica2Corregida/model/enlaces.php
+++ Practica2Corregida/model/enlaces.php
@@ -1,17 +1,24 @@
 <?php 
 class Paginas{
 	public function enlacesPaginasModel($enlaces){
-		if($enlaces == "ingresar" || $enlaces == "usuarios" || $enlaces == "editar" || $enlaces == "salir"){
+		if($enlaces == "ingresar" || $enlaces == "usuarios" || $enlaces == "editar" || $enlaces == "salir"
+		|| $enlaces == "carreras" || $enlaces == "registrarCarrera" || $enlaces == "editarCarrera" || $enlaces == "materias"
+		|| $enlaces == "registrarMateria" || $enlaces == "editarMateria"){
 			$module =  "view/".$enlaces.".php";
-		} else if($enlaces == "index"){
+		} 
+		else if($enlaces == "index"){
 			$module ="view/registrar.php";
-		}else if($enlaces == "ok"){
+		}
+		else if($enlaces == "ok"){
 			$module ="view/registro.php";
-		}else if($enlaces == "fallo"){
+		}
+		else if($enlaces == "fallo"){
 			$module ="view/ingresar.php";
-		}else if($enlaces == "cambio"){
+		}
+		else if($enlaces == "cambio"){
 			$module ="view/usuarios.php";
-		}else{
+		}
+		else{
 			$module ="view/registrar.php";
 		}
 		return $module;
