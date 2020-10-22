5063f243a5031aef314550ae578712200d6d8fca
RubénDaríoRamírezPineda <46793054+Broadcast123@users.noreply.github.com>
2020-10-22 15:54:02 -0500

    Actualizacion4RutasYboostrap_Practica4_Laravel

--- Proyecto_4/app/Http/Controllers/RecetaController.php
+++ Proyecto_4/app/Http/Controllers/RecetaController.php
@@ -9,14 +9,13 @@ class RecetaController extends Controller
     //
     public function __invoke()
     {
-        //Consulta n 1, envio del array "recetas" a la vista
-        $categorias = ['Comida mexicana', 'Comida argentina', 'Postres'];
+        $receta = [
+            'nombre' => 'Pizza Hawaina',
+            'descripcion' => 'Rica pizza de X marca'
+        ];
 
-        //Pasar a la vista el array (sintaxis 1):
-        //Retornar a la vista recetas/index
-        return view('recetas.index')
-                -> with('recetas'.$recetas)
-                -> with('categorias'.$categorias);
+        return $receta;
+                
 
         //Pasar a la vista el array (sintaxis 2):
         //return view ('recetas.index, compact ('recetas', 'categorias'))
--- Proyecto_4/routes/api.php
+++ Proyecto_4/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Ruta para invocar el controlador con el array de las recetas
+
+route::get('/ruta','App\Http\Controllers\RecetaController');
