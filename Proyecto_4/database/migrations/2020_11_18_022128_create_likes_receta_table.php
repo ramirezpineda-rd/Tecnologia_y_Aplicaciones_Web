bb85ddba917eb1aff604f15e082a30ab743a244c
RubénDaríoRamírezPineda <46793054+Broadcast123@users.noreply.github.com>
2020-11-17 23:05:16 -0600

    ExamenUnidad3_Practica4_Laravel_

--- Proyecto_4/database/migrations/2020_11_18_022128_create_likes_receta_table.php
+++ Proyecto_4/database/migrations/2020_11_18_022128_create_likes_receta_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLikesRecetaPivotTable extends Migration
+class CreateLikesRecetaTable extends Migration
 {
     /**
      * Run the migrations.
--- Proyecto_4/database/migrations/2020_11_18_022436_create_perfiles_table.php
+++ Proyecto_4/database/migrations/2020_11_18_022436_create_perfiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePerfilsTable extends Migration
+class CreatePerfilesTable extends Migration
 {
     /**
      * Run the migrations.
